44ae7343dea80acfa0f59f8197a67046fce55c23
MOUKHAFI Anass <moukhafi.anass@gmail.com>
2022-11-12 15:20:36 +0200

    Added a simple calculator with its tests

---
+++ src/org/mql/java/model/Calculator.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package org.mql.java.model;
+
+/**
+ * @author MOUKHAFI Anass
+ *
+ * Nov 12, 2022
+ */
+public class Calculator {
+	
+	public int add(int x, int y) {
+		return x+y;
+	}
+	
+	public double add(double x, double y) {
+		return x+y;
+	}
+	
+	public double substruct(int x, int y) {
+		return x-y;
+	}
+	
+	public int multiply(int x, int y) {
+		return x*y;
+	}
+	
+	public double divide(int x, int y) throws Exception {
+		if(y==0)
+			throw new Exception("Oups, Division by 0 !");
+		return (double)x/y;
+	}
+	
+	/* For integrity :
+	 * 		Reciprocal of the fraction x/y is y/x, in order to make sure
+	 * our methods behave the same way, this method will call divide.
+	 * */
+	public double reciprocal(int x, int y) throws Exception {
+		return divide(y, x);
+	}
+}
---
+++ src/org/mql/java/tests/CalculatorTest.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+package org.mql.java.tests;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mql.java.model.Calculator;
+
+/**
+ * @author MOUKHAFI Anass
+ *
+ * Nov 12, 2022
+ */
+public class CalculatorTest {
+	// Our system under test
+	private Calculator calculator;
+	@BeforeEach
+	void setUp() {
+		calculator = new Calculator();
+	}
+	
+	@Test
+	void shouldAddTwoIntegers() {
+		assertEquals(5, calculator.add(3, 2));
+		assertEquals(-1, calculator.add(-3, 2));
+		assertEquals(11, calculator.add(14, -3));
+	}
+	
+	@Test
+	void shouldAddTwoDoubles() {
+		assertEquals(6.1, calculator.add(3.3, 2.8));
+		assertEquals(-1.1, calculator.add(-3.5, 2.4));
+		assertEquals(10.6, calculator.add(14.2, -3.6));
+	}
+	
+	@Test
+	void shouldSubstructIntegerFromAnother() {
+		assertEquals(1, calculator.substruct(3, 2));
+		assertEquals(-5, calculator.substruct(-3, 2));
+		assertEquals(17, calculator.substruct(14, -3));
+	}
+	
+	@Test
+	void shouldMultiplyTwoIntegers() {
+		assertEquals(12, calculator.multiply(2, 6));
+		assertEquals(0, calculator.multiply(3, 0));
+		assertEquals(42, calculator.multiply(14, 3));
+	}
+	
+	@Test
+	void shouldDivideIntegerByAnother() {
+		assertAll(
+				() -> assertEquals(2.5, calculator.divide(5, 2)),
+				() -> assertEquals(0.75, calculator.divide(3, 4)),
+				() -> assertThrows(Exception.class, ()->{
+					calculator.divide(3, 0);
+				})
+			);
+	}
+	
+	@Test
+	void shouldReturnReciprocalOfAFraction() {
+		assertAll(
+				() -> assertEquals(2.5, calculator.reciprocal(5, 2)),
+				() -> assertEquals(0.75, calculator.reciprocal(3, 4)),
+				() -> assertThrows(Exception.class, ()->{
+					calculator.reciprocal(0, 3);
+				})
+			);
+	}
+	
+	@AfterEach
+	void tearDown() {
+		calculator = null;
+	}
+}
