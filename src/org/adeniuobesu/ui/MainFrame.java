26dff62d1b6fcf5ec50d9c9dc9568eea596ba2f3
MOUKHAFI Anass <moukhafi.anass@gmail.com>
2022-11-12 02:19:15 +0200

    Initialized project and created main frame class

---
+++ src/org/adeniuobesu/ui/MainFrame.java
@@ -0,0 +1,16 @@
+package org.adeniuobesu.ui;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+	private static final long serialVersionUID = 1l;
+	
+	public MainFrame() {
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
+	public static void main(String[] args) {
+		new MainFrame();
+	}
+}
