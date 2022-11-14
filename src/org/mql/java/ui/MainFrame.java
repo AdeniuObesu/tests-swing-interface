e6a1b97503408af472491b915bc5a304153c1135
MOUKHAFI Anass <moukhafi.anass@gmail.com>
2022-11-14 13:36:00 +0200

    Set Windowslook&Feel in our frame

--- src/org/mql/java/ui/MainFrame.java
+++ src/org/mql/java/ui/MainFrame.java
@@ -1,11 +1,17 @@
 package org.mql.java.ui;
 
 import javax.swing.JFrame;
+import javax.swing.UIManager;
 
 public class MainFrame extends JFrame {
 	private static final long serialVersionUID = 1l;
 	
 	public MainFrame() {
+		try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 		setTitle("UNIT TEST RUNNER!");
 		add(new CalculatorTabbedPane());
 		setLocationRelativeTo(null);
